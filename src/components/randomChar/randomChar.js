a0efd7adf5ac95fdfcd213017bbbbbdf75c2e547
Abay <abayibrayev7@gmail.com>
2020-04-18 18:39:17 +0100

    added a spinning loader which is displayed while data is being loaded

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./randomChar.css";
 import GotService from "../../services/gotService";
+import Spinner from "../spinner";
 
 export default class RandomChar extends Component {
   constructor() {
@@ -12,10 +13,11 @@ export default class RandomChar extends Component {
 
   state = {
     char: {},
+    loading: true,
   };
 
   onCharLoaded = (char) => {
-    this.setState({ char });
+    this.setState({ char, loading: false });
   };
 
   updateChar() {
@@ -24,31 +26,37 @@ export default class RandomChar extends Component {
   }
 
   render() {
-    const {
-      char: { name, gender, born, died, culture },
-    } = this.state;
-    return (
-      <div className="random-block rounded">
-        <h4>Random Character: {name}</h4>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Gender </span>
-            <span>{gender}</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Born </span>
-            <span>{born}</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Died </span>
-            <span>{died}</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Culture </span>
-            <span>{culture}</span>
-          </li>
-        </ul>
-      </div>
-    );
+    const { char, loading } = this.state;
+    const content = loading ? <Spinner /> : <View char={char} />;
+
+    return <div className="random-block rounded">{content}</div>;
   }
 }
+
+const View = ({ char }) => {
+  const { name, gender, born, died, culture } = char;
+
+  return (
+    <>
+      <h4>Random Character: {name}</h4>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item d-flex justify-content-between">
+          <span className="term">Gender </span>
+          <span>{gender}</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between">
+          <span className="term">Born </span>
+          <span>{born}</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between">
+          <span className="term">Died </span>
+          <span>{died}</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between">
+          <span className="term">Culture </span>
+          <span>{culture}</span>
+        </li>
+      </ul>
+    </>
+  );
+};
