c1ee95218e80dcce145dd14af1a827b7147c7003
Abay <abayibrayev7@gmail.com>
2020-04-24 20:55:40 +0100

    applying react patterns

--- src/components/charDetails/charDetails.js
+++ src/components/charDetails/charDetails.js
@@ -4,6 +4,16 @@ import GotService from "../../services/gotService";
 import Spinner from "../spinner";
 import ErrorMessage from "../errorMessage";
 
+const Field = ({ char, field, label }) => {
+  return (
+    <li className="list-group-item d-flex justify-content-between">
+      <span className="term">{label}</span>
+      <span>{char[field]}</span>
+    </li>
+  );
+};
+export { Field };
+
 export default class CharDetails extends Component {
   gotService = new GotService();
 
@@ -56,7 +66,7 @@ export default class CharDetails extends Component {
       return <span className="select-error">Please select an item</span>;
     }
 
-    const { name, gender, born, died, culture } = char;
+    const { name } = char;
 
     if (loading) {
       return (
@@ -70,22 +80,9 @@ export default class CharDetails extends Component {
       <div className="char-details rounded">
         <h4>{name}</h4>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Gender</span>
-            <span>{gender}</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Born</span>
-            <span>{born}</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Died</span>
-            <span>{died}</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Culture</span>
-            <span>{culture}</span>
-          </li>
+          {React.Children.map(this.props.children, (child) => {
+            return React.cloneElement(child, { char });
+          })}
         </ul>
       </div>
     );
--- src/components/characterPage/characterPage.js
+++ src/components/characterPage/characterPage.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { Col, Row } from "reactstrap";
 import ItemList from "../itemList";
-import CharDetails from "../charDetails";
+import CharDetails, { Field } from "../charDetails";
 import ErrorMessage from "../errorMessage";
 import GotService from "../../services/gotService";
-import RowBlock from '../rowBlock'
+import RowBlock from "../rowBlock";
 export default class CharacterPage extends Component {
   gotService = new GotService();
 
@@ -40,7 +40,12 @@ export default class CharacterPage extends Component {
       />
     );
 
-    const charDetails = <CharDetails charId={selectedChar} />;
+    const charDetails = (
+      <CharDetails charId={selectedChar}>
+        <Field field="gender" label="Gender" />
+        <Field field="born" label="Born" />
+      </CharDetails>
+    );
 
     return <RowBlock left={itemList} right={charDetails} />;
   }
